c63e4c59af32cba988f850c7941e43c4c29fcab0
Abugre Abayeta <abayetaabugre@gmail.com>
2024-10-07 15:06:27 +0000

    Added footer

--- src/pages/bookDetails/index.jsx
+++ src/pages/bookDetails/index.jsx
@@ -2,7 +2,10 @@ import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 
 const BookDetails = () => {
-  return <div>BookDetails</div>;
+  return <div>
+    <Navbar/>
+    <Footer/>
+  </div>;
 };
 
 export default BookDetails;
