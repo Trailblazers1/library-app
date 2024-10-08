f89c75afff36c509a410eed4bffbc6811dfe593d
mawuse-tech <akpekemawuse1@gmail.com>
2024-10-08 11:23:36 +0000

    added color

--- src/pages/bookList/components/Hero.jsx
+++ src/pages/bookList/components/Hero.jsx
@@ -3,15 +3,15 @@
 const Hero = () => {
   return (
     <div className="font-serif">
-      <div className="bg-[#D0E1E7] w-full h-full">
+      <div className="bg-[#e4ebf0] w-full h-full">
         
          <div className="overlay">
          <div className="py-[35vh]">
-          <h1 className="text-[70px] text-start font-extrabold text-[#829263]">
+          <h1 className="text-[70px] text-start font-extrabold text-[#334412]">
             Get Your New Book With The Best Price</h1>
           <input type="text" 
                 placeholder="Search for a book" 
-                className="pl-10 pr-4 py-2 w-full rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                className="pl-10 pr-4 py-2 w-full  border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#E56F47]" />
         </div>
           {/* <img src={picc} alt="" /> */}
           <img src={pic} alt="" />
@@ -19,6 +19,11 @@ const Hero = () => {
         </div> 
 
       </div>
+
+      <section className="sec">
+      
+
+      </section>
     </div>
   );
 };
