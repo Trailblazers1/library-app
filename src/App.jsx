f5b5e33dbf523a87d6997d24aa19ff46b63fcaff
Abugre Abayeta <abayetaabugre@gmail.com>
2024-10-10 15:35:16 +0000

    Rectified errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
- import BookList from "./pages/bookList";
+import BookList from "./pages/bookList";
 import AddBook from "./pages/addBook";
 import BookDetails from "./pages/bookDetails";
 import About from "./pages/about";
 import AuthForm from "./pages/authForm";
 import WishList from "./pages/wishList";
 import ContactUs from "./pages/contactUs";
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n/i18n'
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n/i18n";
 import EditBook from "./pages/editBook";
 import List from "./pages/getBooks";
-
-
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-       element: <BookList />,
+      element: <BookList />,
     },
 
     {
@@ -48,27 +46,18 @@ function App() {
 
     {
       path: "/contactUs",
-      element: <ContactUs/>
-    },
-
-    <I18nextProvider i18n={i18n}>
-      <div>
-        <h1>{i18n.t('welcome')}</h1>
-        <BookList />
-      </div>
-    </I18nextProvider>
-      element: <About/>
+      element: <ContactUs />,
     },
 
     {
       path: "/editbook",
-      element: <EditBook/>
+      element: <EditBook />,
     },
 
     {
       path: "/getbooks",
-      element: <List/>
-    }
+      element: <List />,
+    },
   ]);
   return <RouterProvider router={router} />;
 }
